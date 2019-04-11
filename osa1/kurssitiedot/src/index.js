fc13f6618d5ba0a223ed88346fc30dd36000e61f
Esa Oksman <esa.oksman@gmail.com>
2019-04-11 16:41:42 +0300

    Add exercise 1.3

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
+  const part1 = {
+      name: 'Reactin perusteet',
+      exercises: 10
+  }
+  const part2 = {
+      name: 'Tiedonvälitys propseilla',
+      exercises: 7
+  }
+  const part3 = {
+      name: 'Komponenttien tila',
+      exercises: 14
+  }
 
   return (
     <div>
         <Header course={course} />
-        <Content exercises1={exercises1} part1={part1} exercises2={exercises2} part2={part2} exercises3={exercises3} part3={part3} />
-        <Total total={exercises1 + exercises2 + exercises3} />
+        <Content part1={part1} part2={part2} part3={part3} />
+        <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
@@ -30,9 +36,9 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <div>
-            <Part part={props.part1} exercises={props.exercises1} />
-            <Part part={props.part2} exercises={props.exercises2} />
-            <Part part={props.part3} exercises={props.exercises3} />
+            <Part part={props.part1.name} exercises={props.part1.exercises} />
+            <Part part={props.part2.name} exercises={props.part2.exercises} />
+            <Part part={props.part3.name} exercises={props.part3.exercises} />
         </div>
     )
 }
