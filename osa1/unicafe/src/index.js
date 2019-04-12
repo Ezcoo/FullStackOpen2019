b65d23f4d872820aa6098adb11b2b0b81ec2050a
Esa Oksman <esa.oksman@gmail.com>
2019-04-12 09:51:32 +0300

    Add 1.8: unicafe step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,51 +1,59 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({handleClick, text}) => (
+const Button = ({ handleClick, text }) => (
     <button onClick={handleClick}>
         {text}
     </button>
 )
 
+const Statistics = (props) => {
+    return (
+        <div>
+            <h1>statistiikka</h1>
+            <p>hyvä {props.good}</p>
+            <p>neutraali {props.neutral}</p>
+            <p>huono {props.bad}</p>
+            <p>yhteensä {props.good + props.neutral + props.bad}</p>
+            <p>keskiarvo {(props.good - props.bad) / props.feedbackTotal}</p>
+            <p>positiivisia {Math.round(props.good / props.feedbackTotal * 100)} %</p>
+        </div>
+    )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [feedbackTotal, setfeedbackTotal] = useState(0)
+    // tallenna napit omaan tilaansa
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    const [feedbackTotal, setfeedbackTotal] = useState(0)
 
-const handleGoodClick = () => {
-    setGood(good + 1)
-    setfeedbackTotal(feedbackTotal + 1)
+    const handleGoodClick = () => {
+        setGood(good + 1)
+        setfeedbackTotal(feedbackTotal + 1)
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+        setfeedbackTotal(feedbackTotal + 1)
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+        setfeedbackTotal(feedbackTotal + 1)
+    }
+
+    return (
+        <div>
+            <h1>anna palautetta</h1>
+            <Button handleClick={handleGoodClick} text='hyvä' />
+            <Button handleClick={handleNeutralClick} text='neutraali' />
+            <Button handleClick={handleBadClick} text='huono' />
+            <Statistics good={good} neutral={neutral} bad={bad} feedbackTotal={feedbackTotal} />
+        </div>
+    )
 }
 
-const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-    setfeedbackTotal(feedbackTotal + 1)
-}
-
-const handleBadClick = () => {
-    setBad(bad + 1)
-    setfeedbackTotal(feedbackTotal + 1)
-}
-
-  return (
-    <div>
-      <h1>anna palautetta</h1>
-      <Button handleClick={handleGoodClick} text='hyvä' />
-      <Button handleClick={handleNeutralClick} text='neutraali' />
-      <Button handleClick={handleBadClick} text='huono' />
-      <h1>statistiikka</h1>
-      <p>hyvä {good}</p>
-      <p>neutraali {neutral}</p>
-      <p>huono {bad}</p>
-      <p>yhteensä {good + neutral + bad}</p>
-      <p>keskiarvo {(good - bad) / feedbackTotal}</p>
-      <p>positiivisia {Math.round(good / feedbackTotal * 100)} %</p>
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, 
-  document.getElementById('root')
+ReactDOM.render(<App />,
+    document.getElementById('root')
 )
